d6276a46986b07150e1b738b46f31f8d30af1504
Ibrahim Khalil <ibrahimkholil01@gmail.com>
2025-02-20 00:14:24 +0600

    okk

---
+++ Controllers/WMCAController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WMCA_Base_Data_Collection.Controllers
+{
+    public class WMCAController : Controller
+    {
+        // GET: WMCA_Dashboard
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
